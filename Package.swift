693a5f52834fe11d24305cf3c56d27dee4624d4d
Ahmet Sina Ustem <asend.ustem@gmail.com>
2020-05-16 21:11:23 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+import PackageDescription
+
+let package = Package(
+    name: "Spring",
+    platforms: [
+        .iOS(.v8), .tvOS(.v11)
+    ],
+    products: [
+        .library(name: "Spring", targets: ["Spring"])
+    ],
+    targets: [
+        .target(name: "Spring", path: "Spring"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
